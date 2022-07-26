f829d559b13e091bbb66021bce49694efedc977f
fhossain <farhana.hossainse@gmail.com>
2022-07-26 19:53:29 -0400

    multer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-//import styled from "styled-components";
-import Items from "../components/Item/Items";
+import styled from 'styled-components'
+import Items from '../components/Item/Items'
 
 const Home = ({ items, updateItemState, user }) => {
   return (
