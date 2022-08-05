8fa8c027e224595491d1801bedd5bc7e782173a3
justaples <101436947+justaples@users.noreply.github.com>
2022-08-05 15:10:47 -0400

    fixing a tah issue

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -43,8 +43,8 @@ const Navbar = ({ user, handleLogout }) => {
               d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"
             />
           </svg>
-          <Link to="/"> Home </Link>
         </a>
+          <Link to="/"> Home </Link>
       </li>
 
       <li id="nav">
@@ -59,8 +59,8 @@ const Navbar = ({ user, handleLogout }) => {
           >
             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
           </svg>
-          <Link to="/new-item"> Trade Plant</Link>
         </a>
+          <Link to="/new-item"> Trade Plant</Link>
       </li>
 
       <li id="nav">
@@ -76,8 +76,8 @@ const Navbar = ({ user, handleLogout }) => {
             <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
             <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2z" />
           </svg>
-          <Link to="/chat"> Chat With Us </Link>
         </a>
+          <Link to="/chat"> Chat With Us </Link>
       </li>
 
       <li id="nav">
@@ -92,8 +92,8 @@ const Navbar = ({ user, handleLogout }) => {
           >
             <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
           </svg>
-          <Link to="/findflorist"> Find Florists </Link>
         </a>
+          <Link to="/findflorist"> Find Florists </Link>
 
         </li>
 
