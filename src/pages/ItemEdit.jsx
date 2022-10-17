3500e703ad7b75af025aa8c125f79531990689fc
fhossain <farhana.hossainse@gmail.com>
2022-10-17 12:03:02 -0400

    new item and edit item cleaned

--- src/pages/ItemEdit.jsx
+++ src/pages/ItemEdit.jsx
@@ -1,13 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-//import styled from "styled-components";
 import "../components/FormStyle/Form.css"
-
-// const StyledForm = styled.form`
-//   display: flex;
-//   flex-direction: column;
-// `;
 
 const ItemEdit = ({ setItems }) => {
   let { id } = useParams();
@@ -55,81 +49,132 @@ const ItemEdit = ({ setItems }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} encType="multipart/form-data">
-      <h1> Edit {formData.name} </h1>
-      
-      <div>
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          name="name"
-          type="text"
-          value={formData.name}
-          onChange={handleChange}
-        />
-      </div>
+    <div class="container-box">
+    <div class="form-container">
+      <div class="image-holder"></div>
+      <form onSubmit={handleSubmit} encType="multipart/form-data">
+        <h2 class="text-center">Edit {formData.name}</h2>
 
-      <div>
-        <label htmlFor="quantity">Quantity</label>
-        <input
-          id="quantity"
-          name="quantity"
-          type="text"
-          value={formData.quantity}
-          onChange={handleChange}
-        />
-      </div>
+        <div class="form-group">
+          <input
+            class="form-control"
+            id="name"
+            placeholder="Name"
+            name="name"
+            type="text"
+            onChange={handleChange}
+value={formData.name}
 
-      <div>
-        <label htmlFor="description">Description</label>
-        <input
-          id="description"
-          name="description"
-          type="text"
-          value={formData.description}
-          onChange={handleChange}
-        />
-      </div>
+          />
+        </div>
 
-      <div>
-        <label htmlFor="genus">Genus</label>
-        <input
-          id="genus"
-          name="genus"
-          type="text"
-          value={formData.genus}
-          onChange={handleChange}
-        />
-      </div>
+        <div class="form-group">
+          <input
+            class="form-control"
+            id="quantity"
+            name="quantity"
+            type="text"
+            placeholder="Quantity"
+            onChange={handleChange}
+value={formData.quantity}
 
-      <div>
-        <label htmlFor="status">Status</label>
-        <input
-          id="status"
-          name="status"
-          type="text"
-          value={formData.status}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="images">Image</label>
-        <input
-          name="images"
-          id="images"
-          type="file"
-          accept="image/png, image/jpeg, image/jpg"
-          onChange={handleFile}
-        />
-      </div>
+          />
+        </div>
 
-      <div className="button">
-        <button id="new" type="submit">
-          {" "}
-          Edit Plant{" "}
-        </button>
-      </div>
-    </form>
+        <div class="form-group">
+          <input
+            class="form-control"
+            id="description"
+            placeholder="Description"
+            name="description"
+            type="text"
+value={formData.description}
+
+            onChange={handleChange}
+          />
+        </div>
+
+        <div class="form-group">
+          <input
+            class="form-control"
+            id="genus"
+            name="genus"
+            placeholder="Genus"
+            type="text"
+            onChange={handleChange}
+value={formData.genus}
+
+          />
+        </div>
+
+        <div class="form-group">
+          <input
+            id="status"
+            placeholder="Status"
+            name="status"
+            type="text"
+            onChange={handleChange}
+            class="form-control"
+value={formData.status}
+
+          />
+        </div>
+
+
+        <div className="mt-1 flex justify-center rounded-md border-2 border-dashed border-black-300 px-6 pt-5 pb-6">
+          <div className="space-y-1 text-center">
+            <svg
+              className="mx-auto h-12 w-12 text-black-400"
+              stroke="currentColor"
+              fill="none"
+              viewBox="0 0 48 48"
+              aria-hidden="true"
+            >
+              <path
+                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <span>Upload Image </span>
+
+            <div className="flex text-sm text-black-600">
+              <label
+                htmlFor="images"
+                className="relative cursor-pointer rounded-md bg-white font-large text-green-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-green-500 focus-within:ring-offset-2 hover:text-green-500"
+              >
+                
+                <input
+                  name="images"
+                
+           
+                  accept="image/png, image/jpeg, image/jpg"
+        
+                  onChange={handleFile}
+                  class="block mb-5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" 
+                  id="default_size" 
+                  type="file"
+                  
+                />
+                  
+
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-black-50 px-4 py-3 text-right sm:px-6">
+          <button
+            type="submit"
+            class="inline-block px-6 py-2.5 bg-green-500 text-white font-large text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out"
+          >
+            Edit Plant
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
   );
 };
 
--- src/pages/NewItem.jsx
+++ src/pages/NewItem.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "../components/FormStyle/Form.css"
-
+import "../components/FormStyle/Form.css";
 
 const NewItem = ({ addItem }) => {
   const initialState = {
@@ -28,7 +27,6 @@ const NewItem = ({ addItem }) => {
     images.append("description", formData.description);
     images.append("genus", formData.genus);
     images.append("status", formData.status);
-    // images.append("file", formData.images);
     console.log(formData);
     axios.post(`http://localhost:8000/item/`, images).then((res) => {
       setFormData(initialState);
@@ -40,147 +38,129 @@ const NewItem = ({ addItem }) => {
   const handleFile = (e) => {
     console.log(e.target);
     setFormData({ ...formData, [e.target.id]: e.target.files[0] });
+
   };
 
   return (
-  //   <form onSubmit={handleSubmit} encType="multipart/form-data">
-  //     <h1>New Trade Item</h1>
-//   <div class="hidden sm:block" aria-hidden="true"> 
-//   <div class="py-5">
-//     <div class="border-t border-gray-200"></div>
-//   </div>
-// </div>
+    <div class="container-box">
+      <div class="form-container">
+        <div class="image-holder"></div>
+        <form onSubmit={handleSubmit} encType="multipart/form-data">
+          <h2 class="text-center">New Plant</h2>
 
-
-{/* <div class="mt-5 md:col-span-2 md:mt-0">
-<form action="#" method="POST">
-  <div class="shadow sm:overflow-hidden sm:rounded-md">
-    <div class="space-y-6 bg-white px-4 py-5 sm:p-6">
-      <div class="grid grid-cols-3 gap-6">
-        <div class="col-span-3 sm:col-span-2">
-          <label for="company-website" class="block text-sm font-medium text-gray-700">Name</label>
-          <div class="mt-1 flex rounded-md shadow-sm">
-            <span class="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500">http://</span>
-            <input type="text" name="name" id="name" class="block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="Name" onChange={handleChange}/>
+          <div class="form-group">
+            <input
+              class="form-control"
+              id="name"
+              placeholder="Name"
+              name="name"
+              type="text"
+              onChange={handleChange}
+            />
           </div>
-        </div>
-      </div> */}
+
+          <div class="form-group">
+            <input
+              class="form-control"
+              id="quantity"
+              name="quantity"
+              type="text"
+              placeholder="Quantity"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div class="form-group">
+            <input
+              class="form-control"
+              id="description"
+              placeholder="Description"
+              name="description"
+              type="text"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div class="form-group">
+            <input
+              class="form-control"
+              id="genus"
+              name="genus"
+              placeholder="Genus"
+              type="text"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div class="form-group">
+            <input
+              id="status"
+              placeholder="Status"
+              name="status"
+              type="text"
+              onChange={handleChange}
+              class="form-control"
+            />
+          </div>
 
 
+          <div className="mt-1 flex justify-center rounded-md border-2 border-dashed border-black-300 px-6 pt-5 pb-6">
+            <div className="space-y-1 text-center">
+              <svg
+                className="mx-auto h-12 w-12 text-black-400"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 48 48"
+                aria-hidden="true"
+              >
+                <path
+                  d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+              <span>Upload Image </span>
 
-  //     <div>
-  //       <label htmlFor="name">Name</label>
-  //       <input id="name" name="name" type="text" onChange={handleChange} />
-  //     </div>
+              <div className="flex text-sm text-black-600">
+                <label
+                  htmlFor="images"
+                  className="relative cursor-pointer rounded-md bg-white font-large text-green-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-green-500 focus-within:ring-offset-2 hover:text-green-500"
+                >
+                  
+                  <input
+                    name="images"
+                  
+             
+                    accept="image/png, image/jpeg, image/jpg"
+          
+                    onChange={handleFile}
+                    class="block mb-5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" 
+                    id="default_size" 
+                    type="file"
+                    
+                  />
+                    
 
-  //     <div>
-  //       <label htmlFor="quantity">Quantity</label>
-  //       <input
-  //         id="quantity"
-  //         name="quantity"
-  //         type="text"
-  //         onChange={handleChange}
-  //       />
-  //     </div>
-
-  //     <div>
-  //       <label htmlFor="description">Description</label>
-  //       <input
-  //         id="description"
-  //         name="description"
-  //         type="text"
-  //         onChange={handleChange}
-  //       />
-  //     </div>
-  //     <div>
-  //       <label htmlFor="genus">Genus</label>
-  //       <input id="genus" name="genus" type="text" onChange={handleChange} />
-  //     </div>
-  //     <div>
-  //       <label htmlFor="status">Status</label>
-  //       <input id="status" name="status" type="text" onChange={handleChange} />
-  //     </div>
-  //     <div>
-  //       <label htmlFor="images">Image</label>
-  //       <input
-  //         name="images"
-  //         id="images"
-  //         type="file"
-  //         accept="image/png, image/jpeg, image/jpg"
-  //         onChange={handleFile}
-  //       />
-  //     </div>
-
-  //     <div className="button"><button id="new" type="submit"> New Plant </button></div>
-//   <div class="hidden sm:block" aria-hidden="true"> 
-//   <div class="py-5">
-//     <div class="border-t border-gray-200"></div>
-//   </div>
-// </div>
-  //   </form>
-  // );
-  <h1>
-
-
-  <div>
-  <div class="md:grid md:grid-cols-3 md:gap-6">
-  <h4 class="text-lg font-medium leading-6 text-gray-900">New Plant</h4>
-
-
-
-            <div>
-              <label for="about" class="block text-sm font-medium text-gray-700">About</label>
-              <div class="mt-1">
-                <textarea id="about" name="about" rows="3" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="you@example.com"></textarea>
-              </div>
-              <p class="mt-2 text-sm text-gray-500">Brief description for your profile. URLs are hyperlinked.</p>
-            </div>
-
-            <div>
-              <label class="block text-sm font-medium text-gray-700">Photo</label>
-              <div class="mt-1 flex items-center">
-                <span class="inline-block h-12 w-12 overflow-hidden rounded-full bg-gray-100">
-                  <svg class="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-                    <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                  </svg>
-                </span>
-                <button type="button" class="ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Change</button>
-              </div>
-            </div>
-
-            <div>
-              <label class="block text-sm font-medium text-gray-700">Cover photo</label>
-              <div class="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
-                <div class="space-y-1 text-center">
-                  <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                    <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                  </svg>
-                  <div class="flex text-sm text-gray-600">
-                    <label for="file-upload" class="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500">
-                      <span>Upload a file</span>
-                      <input id="file-upload" name="file-upload" type="file" class="sr-only" />
-                    </label>
-                    <p class="pl-1">or drag and drop</p>
-                  </div>
-                  <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-                </div>
+                </label>
               </div>
             </div>
           </div>
-          <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-            <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Save</button>
+
+          <div className="bg-black-50 px-4 py-3 text-right sm:px-6">
+            <button
+              type="submit"
+              class="inline-block px-6 py-2.5 bg-green-500 text-white font-large text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out"
+            >
+              Add +
+            </button>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
-  </div>
-</div>
-
-</h1>
-<></>
-)
+  );
 };
 
-
-
 export default NewItem;
+
+//link: https://webdesignerwall.com/wdw-snippet/signup-form-with-image-on-left-side
