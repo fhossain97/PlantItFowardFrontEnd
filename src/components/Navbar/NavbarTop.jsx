e36ee2c219afad62771dea092d092f7bbe1582f3
fhossain <farhana.hossainse@gmail.com>
2022-12-21 17:02:55 -0500

    committed

--- src/components/Navbar/NavbarTop.jsx
+++ src/components/Navbar/NavbarTop.jsx
@@ -13,7 +13,28 @@ const NavbarTop = ({ user, handleLogout }) => {
   }, []);
 
   let nav = user ? (
+
     <div className="page-wrapper">
+    <div className="nav-wrapper">
+      <div className="grad-bar"></div>
+      <nav className="navbar">
+      <ul className="nav">
+        <li className="nav-item">
+          <Link to="/login" style={{textDecoration: 'none'}}> Login
+           
+          </Link>
+        </li>
+        <li className="nav-item">
+        <Link to="/signup" style={{textDecoration: 'none'}}> Signup </Link>
+        </li>
+        </ul>
+      </nav>
+      <div className="grad-bar"></div>
+    </div>
+  </div>
+    
+  ) : (
+<div className="page-wrapper">
       <div className="nav-wrapper">
         <div className="grad-bar"></div>
         <nav className="navbar">
@@ -61,25 +82,6 @@ const NavbarTop = ({ user, handleLogout }) => {
             </li>
 
             <SearchBar data={items} />
-          </ul>
-        </nav>
-        <div className="grad-bar"></div>
-      </div>
-    </div>
-  ) : (
-    <div className="page-wrapper">
-      <div className="nav-wrapper">
-        <div className="grad-bar"></div>
-        <nav className="navbar">
-        <ul className="nav">
-          <li className="nav-item">
-            <Link to="/login" style={{textDecoration: 'none'}}> Login
-             
-            </Link>
-          </li>
-          <li className="nav-item">
-          <Link to="/signup" style={{textDecoration: 'none'}}> Signup </Link>
-          </li>
           </ul>
         </nav>
         <div className="grad-bar"></div>
